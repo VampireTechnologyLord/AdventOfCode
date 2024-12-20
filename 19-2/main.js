d79a4baa52f39548b28e0b6e7ebad24422bc2930
VampireTechnologyLord <elias.freund@ewe.net>
2024-12-20 22:31:20 +0100

    day 19

---
+++ 19-2/main.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+const input = fs
+    .readFileSync("./19-2/input.txt", "utf8")
+    .replaceAll("\r", "")
+    .split("\n\n");
+const availablePatterns = input[0].split(", ");
+const towels = input[1].split("\n");
+const memory = new Map();
+memory.set("", 1);
+
+function populateDataForReoccurringPatterns(towel) {
+    if (memory.has(towel)) {
+        return memory.get(towel);
+    }
+    if (towel === "") {
+        return true;
+    }
+
+    let reoccurringCount = 0;
+    for (const pattern of availablePatterns) {
+        if (towel.startsWith(pattern)) {
+            if (populateDataForReoccurringPatterns(towel.slice(pattern.length))) {
+                reoccurringCount += memory.get(towel.slice(pattern.length));
+            }
+        }
+    }
+    memory.set(towel, reoccurringCount);
+    return reoccurringCount > 0;
+}
+
+let result = 0;
+for (let i = 0; i < towels.length; i++) {
+    const towel = towels[i];
+    populateDataForReoccurringPatterns(towel);
+    result += memory.get(towel);
+}
+
+console.log(result);
